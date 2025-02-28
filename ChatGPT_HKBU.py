36bb22c4644578262fc70b7d54b88738b3bdb94e
Wendee Lau <wendeelau@Wendees-Laptop.local>
2025-02-28 12:02:35 +0800

    version0.3:Improve some details and add some comments

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,21 +1,31 @@
 import configparser
+#a lib for making HTTP requests
 import requests
 
+#define a class
 class HKBU_ChatGPT():
+    #initialize an instance and load my config file
     def __init__(self,config_='./config.ini'):
+        #if config_ is a dir string ,firstly create ConfigParser instance then read the config file
         if type(config_) == str:
             self.config = configparser.ConfigParser()
             self.config.read(config_)
+        #if config_ is already an instance use it directly
         elif type(config_) == configparser.ConfigParser:
             self.config = config_
 
     def submit(self,message):
-        conversation = [{"role":"user","content":message}]
-
-        url=(self.config['CHATGPT']['BASICURL'])+"/deployments/" + (self.config['CHATGPT']['MODELNAME'])+"/chat/completions/?api-version=" +(self.config['CHATGPT']['APIVERSION'])
+        basicurl=self.config['CHATGPT']['BASICURL']
+        modelname=self.config['CHATGPT']['MODELNAME']
+        apiversion=self.config['CHATGPT']['APIVERSION']
+        #form the URL
+        url=basicurl+"/deployments/"+modelname+"/chat/completions/?api-version="+apiversion
         headers={'Content-Type':'application/json',
                  'api-key':(self.config['CHATGPT']['ACCESS_TOKEN'])}
+        #pass the user's message；payload（body）
+        conversation = [{"role": "user", "content": message}]
         payload={'messages':conversation}
+    #using POST HTTP method , pass parameters,send requests to api
         response=requests.post(url,json=payload,headers=headers)
         if response.status_code==200:
             data=response.json()
@@ -23,12 +33,13 @@ class HKBU_ChatGPT():
         else:
             return 'Error:',response
 
-
 if __name__ == '__main__':
+    #an instance of HKBU_ChatGPT CLASS
     ChatGPT_test = HKBU_ChatGPT()
     try:
+        #a loop pass the users input to chatgpt and print response
         while True:
-            user_input = input("Typing anything to ChatGPT:\t")
+            user_input = input("Typing to start your chat :\t")
             response = ChatGPT_test.submit(user_input)
             print(response)
     except KeyboardInterrupt :
--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,3 @@
-from idlelib.configdialog import help_common
-from idlelib.debugobj import dispatch
 #i think here in this case i donnot use the tools of telegram directly
 #import telegram
 from telegram import update, Update
@@ -13,7 +11,8 @@ import redis
 from ChatGPT_HKBU import HKBU_ChatGPT
 
 #global variable redis1
-global redis1
+#redis client
+#global redis1
 
 def main():
     config=configparser.ConfigParser()
@@ -23,6 +22,8 @@ def main():
 #use_context=True : my handler will recieve the Context object as a argument
     updater=Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context=True)
     dispatcher=updater.dispatcher
+
+#a redis client
     global redis1
 #create an instance of Redis
     redis1=redis.Redis(host=(config['REDIS']['HOST']),
@@ -31,14 +32,13 @@ def main():
                        decode_responses=(config['REDIS']['DECODE_RESPONSE']),
                        username=(config['REDIS']['USER_NAME']))
 
-
 #config the format or others for the output log
     logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 #diapater for echo function
 #Filters.text is a filter,~Filters.command is a deny filter to exclude commands text,
 # here combine these 2 filters
-#echo is a function we defined to deal with the messages
+#echo is a callback function (i defined it) to deal with the messages
     #echo_handler=MessageHandler(Filters.text & (~Filters.command),echo)
     #dispatcher.add_handler(echo_handler)
 
@@ -48,19 +48,15 @@ def main():
     chatgpt_handler=MessageHandler(Filters.text & (~Filters.command),equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
 
-    dispatcher.add_handler(CommandHandler("add",add))
-    dispatcher.add_handler(CommandHandler("help",help_command))
-#start bot
+#dispatchers for self defined cmd
+    dispatcher.add_handler(CommandHandler("add",add_cmd))
+    dispatcher.add_handler(CommandHandler("help",help_cmd))
+    dispatcher.add_handler(CommandHandler("hello",hello_cmd))
+    
+#start chatbot
     updater.start_polling()
     updater.idle()
 
-#use ChatGPT API
-def equiped_chatgpt(update,context):
-    global chatgpt
-    reply_message=chatgpt.submit(update.message.text)
-    logging.info("Update:"+str(update))
-    logging.info("context:"+str(context))
-    context.bot.send_message(chat_id=update.effective_chat.id,text=reply_message)
 
 #define the function echo to process the updated messages
 def echo(update,context):
@@ -69,26 +65,48 @@ def echo(update,context):
     logging.info("Context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id,text=reply_message)
 
+# no return value
+#Send a message when the command /help is issue.
+def help_cmd(update:Update,context:CallbackContext):
+    update.message.reply_text("Helping you.")
 
-def help_command(update:Update,context:CallbackContext) -> None:
-    """Send a message when the command /help is issue."""
-    update.message.reply_text("Helping you helping you.")
 
-
-
-def add(update:Update,context:CallbackContext) -> None:
-    """Send a message when the command /add is issue."""
+#no return value
+#Send a message when the command /add is issue
+def add_cmd(update:Update,context:CallbackContext):
     try:
         global redis1
+        # memory the user's input keyword
         logging.info(context.args[0])
-        msg = context.args[0]
-        redis1.incr(msg)
-# get(msg) may not need decode???
-        update.message.reply_text('You have said ' + msg + ' for ' + redis1.get(msg) + ' times.')
-
+        # usr_msg local varieble in this func
+        usr_msg = context.args[0]
+        redis1.incr(usr_msg)
+# get(usr_msg) may not need decode  because decode_responses=True
+        #key in redis is the keywords input by usr
+        #value is the frequency
+        update.message.reply_text('You have said ' + usr_msg + ' for ' + redis1.get(usr_msg) + ' times.')
     except(IndexError,ValueError):
         update.message.reply_text('Usage: /add <keyword>')
 
+#define /hello <name> , no return value
+def hello_cmd(update:Update,context:CallbackContext):
+    try:
+        #here no need redis
+        #memory the user's input name
+        logging.info(context.args[0])
+        #usr_msg local varieble in this func
+        usr_msg = context.args[0]
+        update.message.reply_text('Good day , '+usr_msg+' !')
+    except(IndexError,ValueError):
+        update.message.reply_text('Usage: /hello <your name>')
+
+#use ChatGPT API
+def equiped_chatgpt(update,context):
+    global chatgpt
+    reply_message=chatgpt.submit(update.message.text)
+    logging.info("Update:"+str(update))
+    logging.info("context:"+str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id,text=reply_message)
 
 
 if __name__=='__main__':
